9b92f52ce690f4c3237601e58b1b7edf0f56213f
steve von negri <stevevonnegri@gmail.com>
2022-01-03 13:55:57 +0100

    commit fin

---
+++ WisePoll/WisePoll.Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WisePoll.Data
+{
+    public class ApplicationDbContext
+    {
+    }
+}
---
+++ WisePoll/WisePoll.Services/WiseServices.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WisePoll.Services
+{
+    public class WiseServices
+    {
+    }
+}
