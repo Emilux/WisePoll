58c912b84ffb3eba7463df734ebf4468396c9e21
stevevonnegri <73234541+stevevonnegri@users.noreply.github.com>
2022-01-05 20:56:48 +0100

    end register / login

---
+++ WisePoll/WisePoll/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WisePoll.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
