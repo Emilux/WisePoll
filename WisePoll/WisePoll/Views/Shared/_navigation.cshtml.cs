4f9341b3a5feca507dceca8747a8c869693d8b15
Emilien <emilien.fuchs@ati4group.com>
2022-01-05 17:54:46 +0100

    Added header partial html and css, +3 Adds, +1 Modification
    + Added header partial html and css
    + Added footer partial html and css
    + Added Home page html and css
    + Added Bootstrap scss
    + Modified layout code structure, split navbar, header, footer in different partial html files

---
+++ WisePoll/WisePoll/Views/Shared/_navigation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WisePoll.Views.Shared
+{
+    public class _navigation : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
