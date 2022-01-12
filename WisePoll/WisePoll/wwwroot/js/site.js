fca8ea1126870bda631f655c563b9a830b98b1eb
Emilien <emilien.fuchs@ati4group.com>
2022-01-12 22:38:51 +0100

    Fixed email configuration

--- WisePoll/WisePoll/wwwroot/js/site.js
+++ WisePoll/WisePoll/wwwroot/js/site.js
@@ -7,8 +7,8 @@ $(document).ready(function()
     let inputElm = document.querySelector('.taginput'), 
         tagify = new Tagify (inputElm, {
             delimiters: ",| ",
-            duplicates: true,
+            duplicates: false,
             originalInputValueFormat: valuesArr => valuesArr.map(item => item.value).join(','),
-            //pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
         });
 });
